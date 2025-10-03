973397edd71946090cc46fcb402dc75f802086c9
serene <e1355867@u.nus.edu>
2025-10-03 18:40:39 +0800

    Update RemarkCommandTest

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,17 +1,21 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for RemarkCommand.
+ */
 public class RemarkCommandTest {
 
-    private final Model model = new ModelManager();
-
-    private static final String MESSAGE_NOT_IMPLEMENTED_YET = "This operation is not yet supported.";
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void execute() {
