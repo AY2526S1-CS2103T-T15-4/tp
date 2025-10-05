ca7bb6e7c119dbb2e89ae025d9c696f76c39f8ef
hansonpoh <hansonpoh@gmail.com>
2025-10-05 13:32:52 +0800

    Add JavaDoc to Time

--- src/main/java/seedu/address/model/person/Time.java
+++ src/main/java/seedu/address/model/person/Time.java
@@ -3,6 +3,11 @@ package seedu.address.model.person;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents the Time of Person's country time zone.
+ * The time displayed currently in this version is only
+ * of the OS of the user opening the application.
+ */
 public class Time {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMM HH:mm:ss");
