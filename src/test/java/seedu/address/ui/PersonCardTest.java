c05b5a72096a4d3c8c38d7fdc74a3835f5f52d0a
hansonpoh <hansonpoh@gmail.com>
2025-10-05 14:09:16 +0800

    Fix test case

--- src/test/java/seedu/address/ui/PersonCardTest.java
+++ src/test/java/seedu/address/ui/PersonCardTest.java
@@ -1,23 +1,46 @@
 package seedu.address.ui;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import javafx.application.Platform;
+import seedu.address.model.person.Company;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.HomeCountry;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
-import seedu.address.model.util.SampleDataUtil;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
 
 public class PersonCardTest {
 
     @BeforeAll
     static void initToolkit() {
-        Platform.startup(() -> {});
+        Platform.startup(() -> {
+        });
     }
 
     @Test
     void constructor_shouldRunWithoutException() {
-        Person person = SampleDataUtil.getSamplePersons()[0];
+        // Create a local modifiable set
+        Set<Tag> tags = new HashSet<>();
+        tags.add(new Tag("friend"));
+
+        Person person = new Person(
+                new Name("Alice"),
+                new Phone("12345678"),
+                new Email("alice@example.com"),
+                new HomeCountry("Singapore"),
+                new Company("ACME Corp"),
+                tags
+        );
+
+        // Just creating the PersonCard
         new PersonCard(person, 1);
+
     }
 
 }
