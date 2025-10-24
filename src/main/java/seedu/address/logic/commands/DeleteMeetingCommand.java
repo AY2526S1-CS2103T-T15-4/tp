3415efbffc36344f2d214435bf671db2c79042be
Meckss <maxkhoozhengning1@gmail.com>
2025-10-24 15:29:52 +0800

    Fix checkstyle issue

--- src/main/java/seedu/address/logic/commands/DeleteMeetingCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteMeetingCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
-import static seedu.address.logic.Messages.format;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MEETING;
 
 import java.time.LocalDateTime;
