4b083bae24658b703bcfaf2e5cb384c670279abd
serene <e1355867@u.nus.edu>
2025-11-02 21:26:33 +0800

    Fix link to not reset back to original list

--- src/main/java/seedu/address/logic/commands/LinkCommand.java
+++ src/main/java/seedu/address/logic/commands/LinkCommand.java
@@ -73,7 +73,6 @@ public class LinkCommand extends Command {
         }
 
         model.setPerson(personToEdit, editedPerson);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
 
         String resultMessage;
 
