9c15e843ed76a5c147018c19b4c7abe878803e1f
Javier-Lua <javier_lua@outlook.com>
2025-11-03 02:17:49 +0800

    Fix test cases

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -30,12 +30,14 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("91")); // less than 3 numbers
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
-        assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
         assertFalse(Phone.isValidPhone("123456789012345678901234567890")); // too long digits
 
         // valid phone numbers
         assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
+        assertTrue(Phone.isValidPhone("9312 1534")); // spaces within digits
         assertTrue(Phone.isValidPhone("93121534"));
+        assertTrue(Phone.isValidPhone("+6593121534"));
+        assertTrue(Phone.isValidPhone("931-215-34"));
         assertTrue(Phone.isValidPhone("124293842033123")); // long phone numbers
     }
 
